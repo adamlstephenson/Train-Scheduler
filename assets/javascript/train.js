a5ab063a1d78908a2ce6dee66af9bf870986d95a
Adam Stephenson <astephensonx@gmail.com>
2018-03-27 21:20:25 -0500

    initialized Firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAmyfBh_iUuUqp_QvyBa8IAvgXZJeCUWWI",
+    authDomain: "train-scheduler-e7ae0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e7ae0.firebaseio.com",
+    projectId: "train-scheduler-e7ae0",
+    storageBucket: "train-scheduler-e7ae0.appspot.com",
+    messagingSenderId: "22589334577"
+  };
+  firebase.initializeApp(config);
